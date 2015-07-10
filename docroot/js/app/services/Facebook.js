ede344a521a851c59045e30e637d0767a45b43bc
Robert Gungor <robertgungor@Roberts-MacBook-Pro.local>
2015-07-10 02:00:13 -0500

    yes

--- docroot/js/app/services/Facebook.js
+++ docroot/js/app/services/Facebook.js
@@ -112,7 +112,7 @@ define(["jquery", "backbone", "models/App", "text!templates/share-facebook.html"
                     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
                     if (d.getElementById(id)) {return;}
                     js = d.createElement('script'); js.id = id; js.async = true;
-                    js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js#xfbml=1&appId="+self.model.config.fbcAppKey;
+                    js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js#xfbml=1&appId="+self.config.fbcAppKey;
                     ref.parentNode.insertBefore(js, ref);
 
                 }(document, /*debug*/ false));
