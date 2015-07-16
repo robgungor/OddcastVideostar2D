e9dfe437d68eef811153ace0832809b8f09f88cd
Robert Gungor <robertgungor@Roberts-MacBook-Pro.local>
2015-07-16 02:22:33 -0500

    yeah

--- docroot/js/app/utils/OC_Uploader.js
+++ docroot/js/app/utils/OC_Uploader.js
@@ -14,7 +14,7 @@ define(['backbone', 'underscore', 'utils/OC_Utils', 'utils/OC_Parser'],
 		Uploads base64Encoded file and gets a temp location url
 		*/
 		upload_V3 : function (base64File, callback) {
-			var tmp = OC_Utilities.getUrl( "//" + OC_CONFIG.baseURL + "/api/upload_v3.php?extension=png&convertImage=true&sessId=" +this.__getSessionId(), {FileDataBase64:base64File}, callback);
+			var tmp = OC_Utilities.getUrl( "//" + OC_CONFIG.baseURL + "/api/upload_v3.php?extension=png&convertImage=true&sessId=" +this.__getSessionId(), {FileDataBase64:base64File}, false, callback);
 			if(tmp!="OK"){
 			  //errorCaught(null, "upload_v3.php: " +tmp);
 			  alert("ERROR");
