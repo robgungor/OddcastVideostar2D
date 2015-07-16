8427383ead913fd02d2b7ad564bb85e8b3f604b2
Robert Gungor <robertgungor@Roberts-MacBook-Pro.local>
2015-07-16 02:36:17 -0500

    yeah

--- docroot/js/app/views/Positioning.js
+++ docroot/js/app/views/Positioning.js
@@ -170,10 +170,12 @@ define(["jquery", "backbone", "models/App", "text!templates/positioning.html", "
         var _base64Im = _canvas.toDataURL();
         _base64Im = _base64Im.substring(_base64Im.indexOf(",")+1);
         var head = self.model.heads.currentHead;
-        head.set({'src':OC_Uploader.upload_V3(_base64Im, function(){
+        var src = OC_Uploader.upload_V3(_base64Im, function(){
           $('#main-loading-spinner').fadeOut(300);
           window.router.navigate('upload-manager', true);
-        })});
+        });
+        console.log('SRC: '+src);
+        head.set({'src':src});
         
         
         //setTimeout(function(){ self.createFinalSharedVideo(); }, 100);
